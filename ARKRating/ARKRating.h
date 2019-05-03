f1b5262a9f2053e0d8fc1f101387c47826ba9a0e
Aswin Raj Kharel <query.ark@gmail.com>
2019-05-03 16:46:28 -0500

    Initial Commit

---
+++ ARKRating/ARKRating.h
@@ -0,0 +1,19 @@
+//
+//  ARKRating.h
+//  ARKRating
+//
+//  Created by Aswin Raj Kharel on 5/3/19.
+//  Copyright © 2019 Aswin Raj Kharel. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ARKRating.
+FOUNDATION_EXPORT double ARKRatingVersionNumber;
+
+//! Project version string for ARKRating.
+FOUNDATION_EXPORT const unsigned char ARKRatingVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ARKRating/PublicHeader.h>
+
+
